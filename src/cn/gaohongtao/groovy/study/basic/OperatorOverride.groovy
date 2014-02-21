654116ad12450d4bd206b7345de3c27fdac6c182
hanahmily from ubuntu <hanahmily@gmail.com>
2014-02-21 15:40:40 +0800

    Initial commit

---
+++ src/cn/gaohongtao/groovy/study/basic/OperatorOverride.groovy
@@ -0,0 +1,60 @@
+class Man {
+    private boolean sex;
+
+    Man(boolean sex) {
+        this.sex = sex;
+    }
+
+    List<Man> plus(Man man) {
+        //异性生个娃
+        if (man.sex != this.sex) {
+            return [man, this, new Man(man.sex)];
+        } else {
+            //同性俩人自己过
+            return [man, this];
+        }
+    }
+}
+
+def man = new Man(true);
+def woman = new Man(false);
+
+assert 3 == (man + woman).size();
+
+def anotherMan = new Man(true);
+
+assert 2 == (man + anotherMan).size();
+//重载列表
+/*表 1. Groovy 的可重载操作符
+1.	比较操作符对应着普通的 Java equals 和 compareTo 实现
+2.	Java 的算术类操作符，例如 +、- 和 *
+3.	数组存取类操作符 []
+
+表 2. 比较操作符  is()作为引用相等
+操作符	方法
+a == b	a.equals(b)
+a <=> b	a.compareTo(b)
+
+表 3. 四个附加的值
+操作符	含义
+a > b	如果 a.compareTo(b) 返回的值大于 0，那么这个条件为 true
+a >= b	 如果 a.compareTo(b) 返回的值大于等于 0，那么这个条件为 true
+a < b	如果 a.compareTo(b) 小于 0，那么这个条件为 true
+a <= b	如果 a.compareTo(b) 小于等于 0，那么这个条件为 true
+
+表 3. Groovy 的算术类操作符
+操作符	方法
+a + b	 a.plus(b)
+a - b	 a.minus(b)
+a * b	 a.multiply(b)
+a / b	 a.divide(b)
+a++ or ++a	 a.next()
+a-- or --a	 a.previous()
+a << b	 a.leftShift(b)
+
+表 4. 数组操作符
+操作符	方法
+a[b]	 a.getAt(b)
+a[b] = c	 a.putAt(b, c)
+
+*/
